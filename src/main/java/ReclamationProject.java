deaaa08fdad8e63a677aa965dd2b5c0ff520dd55
wyc20 <wyc20@DESKTOP-O981VA2>
2017-09-26 14:48:19 -0500

    good

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -8,19 +8,29 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
+public class ReclamationProject {
+    static String doit( String a, String b) {
+        if (a.length() > b.length()) {
             String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+            a = b;
+            b = c;
+            }
+        if ((a.equals(b))) {
+            string r = "\" : \""; // I love the ternary operator!
+        }
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
-}
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                        r = a.substring(i, i + j);
+                    } else {
+                        r = r;
+                }  // Ah yeah
+        }
+        return r;
+        }
+      }
+    }
